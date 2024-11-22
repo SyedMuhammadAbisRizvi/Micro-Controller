88423ec32a490e771add910a853f6883c3fc021b
SyedMuhammadAbisRizvi <82658565+SyedMuhammadAbisRizvi@users.noreply.github.com>
2024-11-22 14:44:03 +0100

    task_1.ino

--- Task1/task_1.ino
+++ Task1/task_1.ino
@@ -1,53 +1,60 @@
+// Define the state of the traffic light
 enum Status { green, yellow, red };
 Status status = green;
-int set;
+
+const int T20 = 20;  // Each "time event" is 20 milliseconds
+
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+int timeEvent = 0; // This will count the number of T20 events
 
 void setup() {
-  pinMode(6, OUTPUT); // Green LED
-  pinMode(4, OUTPUT); // Yellow LED
-  pinMode(2, OUTPUT); // Red LED
+  pinMode(6, OUTPUT);  // Green LED
+  pinMode(4, OUTPUT);  // Yellow LED
+  pinMode(2, OUTPUT);  // Red LED
 }
 
 void loop() {
-  static unsigned long lastMillis = 0;
-  unsigned long currentMillis = millis();
+  currentMillis = millis();  // Get the current time
+
+  // Count the number of T20 events
+  if (currentMillis - previousMillis >= T20) {
+    previousMillis = currentMillis;
+    timeEvent++;
+  }
 
   switch (status) {
     case green:
-      if (currentMillis - lastMillis >= 3000) {
-        lastMillis = currentMillis;
-        digitalWrite(6, LOW); // Turn off green light
+      // Green light should stay on for 3 seconds (150 T20 events)
+      if (timeEvent >= 150) {
+        digitalWrite(6, LOW);  // Turn off green light
         digitalWrite(4, HIGH); // Turn on yellow light
-        status = yellow;
-        set = 1;
+        status = yellow; // Transition to yellow
+        timeEvent = 0;  // Reset time event counter
       } else {
         digitalWrite(6, HIGH); // Keep green light on
       }
       break;
 
     case yellow:
-      if (currentMillis - lastMillis >= 1000) {
-        lastMillis = currentMillis;
+      // Yellow light should stay on for 1 second (50 T20 events)
+      if (timeEvent >= 50) {
         digitalWrite(4, LOW); // Turn off yellow light
-        if (set == 1) {
-          digitalWrite(2, HIGH); // Turn on red light
-          status = red;
-        } else {
-          digitalWrite(6, HIGH); // Turn on green light
-          status = green;
-        }
+        digitalWrite(2, HIGH); // Turn on red light
+        status = red; // Transition to red
+        timeEvent = 0;  // Reset time event counter
       } else {
         digitalWrite(4, HIGH); // Keep yellow light on
       }
       break;
 
     case red:
-      if (currentMillis - lastMillis >= 3000) {
-        lastMillis = currentMillis;
+      // Red light should stay on for 3 seconds (150 T20 events)
+      if (timeEvent >= 150) {
         digitalWrite(2, LOW); // Turn off red light
         digitalWrite(4, HIGH); // Turn on yellow light
-        status = yellow;
-        set = 0;
+        status = yellow; // Transition to yellow
+        timeEvent = 0;  // Reset time event counter
       } else {
         digitalWrite(2, HIGH); // Keep red light on
       }
