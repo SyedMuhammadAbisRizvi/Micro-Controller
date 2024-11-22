b69963b0f21f8f2be0afd7041ad4339551f0fd14
SyedMuhammadAbisRizvi <82658565+SyedMuhammadAbisRizvi@users.noreply.github.com>
2024-11-22 03:20:22 +0100

    Task_2.ino

--- Task 2/task_2.ino
+++ Task 2/task_2.ino
@@ -1,85 +1,112 @@
-enum Status { green, yellow, red, pgreen, pred };  // Traffic light states
-Status status = green;  // Initial state is green
-int set = 0;  // Flag to control state transition
-unsigned long lastMillis = 0;  // Variable to store the last time an event was triggered
-unsigned long greenInterval = 10000;  // Green light duration (10 seconds)
-unsigned long yellowInterval = 3000; // Yellow light duration (3 seconds)
-unsigned long redInterval = 10000;   // Red light duration (10 seconds)
-unsigned long additionalTime = 3200; // Additional time for the button press scenario
-unsigned long buttonPressTime = 0;   // Timer for button press scenario
-bool buttonPressed = false;          // Flag to check if the button was pressed
+enum Status {green, yellow, red, pgreen, pred}; // Traffic and pedestrian states
+Status status = green;
+
+unsigned long lastMillis = 0;  // Tracks the last state update
+unsigned long interval = 10000; // Default time interval
+bool pedestrianRequest = false; // Tracks if the pedestrian button is pressed
 
 void setup() {
-  pinMode(12, OUTPUT);  // Green light
-  pinMode(11, OUTPUT);  // Yellow light
-  pinMode(10, INPUT);   // Input for button/sensor
-  pinMode(6, OUTPUT);   // Red light
-  pinMode(4, OUTPUT);   // Yellow warning light
-  pinMode(2, OUTPUT);   // Additional light for green
+  // Traffic light pins
+  pinMode(12, OUTPUT); // Pedestrian red light
+  pinMode(11, OUTPUT); // Pedestrian green light
+  pinMode(10, INPUT);  // Pedestrian button input
+  pinMode(6, OUTPUT);  // Traffic red light
+  pinMode(4, OUTPUT);  // Traffic yellow light
+  pinMode(2, OUTPUT);  // Traffic green light (and pedestrian wait indicator)
 }
 
 void loop() {
-  int change = digitalRead(10);  // Read the button/sensor input
-  unsigned long currentMillis = millis();  // Get the current time
+  unsigned long currentMillis = millis();
 
-  if (change == HIGH) {
-    if (!buttonPressed) {  // Ensure the button press is only handled once
-      buttonPressed = true;  // Set the flag to indicate button press
-      buttonPressTime = currentMillis;  // Store the time of button press
-      // Start the red-yellow transition after 3200ms delay
-    }
+  int buttonState = digitalRead(10); // Read pedestrian button state
+  if (buttonState == HIGH && !pedestrianRequest && status != pgreen) {
+    pedestrianRequest = true;         // Set pedestrian request
+    lastMillis = currentMillis;       // Reset timing
+    status = pgreen;                  // Transition to pedestrian green state
+    interval = 3200;                  // Time to transition traffic lights for pedestrians
+  }
 
-    if (buttonPressed && (currentMillis - buttonPressTime >= additionalTime)) {
-      // After 3200ms, switch to red and yellow lights
-      digitalWrite(6, HIGH);  // Turn on the red light
-      digitalWrite(12, LOW);  // Turn off green
-      digitalWrite(11, HIGH); // Turn on yellow
-      if (currentMillis - lastMillis >= 10000) {
-        // Keep yellow light on for 10 seconds, then switch to green
-        lastMillis = currentMillis;
-        digitalWrite(11, LOW);  // Turn off yellow
-        digitalWrite(12, HIGH); // Turn on green
-        buttonPressed = false;  // Reset the button press flag
+  if (pedestrianRequest) {
+    handlePedestrianLights(currentMillis); // Manage pedestrian lights
+  } else {
+    handleTrafficLights(currentMillis); // Manage traffic lights
+  }
+}
+
+void handleTrafficLights(unsigned long currentMillis) {
+  switch (status) {
+    case green:
+      if (digitalRead(2) == LOW) {
+        digitalWrite(2, HIGH); // Turn on traffic green light
+        digitalWrite(12, HIGH); // Pedestrian red light ON
+        lastMillis = currentMillis; // Reset timing
       }
-    }
-  } else if (change == LOW) {
-    switch (status) {
-      case green:
-        digitalWrite(12, HIGH);  // Green light on
-        digitalWrite(2, HIGH);   // Additional light for green on
-        if (currentMillis - lastMillis >= greenInterval) {
-          lastMillis = currentMillis;  // Store the time of the event
-          digitalWrite(2, LOW);  // Turn off additional green light
-          status = yellow;
-          set = 1;  // Flag for transitioning to red
-        }
-        break;
+      if (currentMillis - lastMillis >= interval) {
+        digitalWrite(2, LOW); // Turn off traffic green light
+        status = yellow;      // Transition to yellow state
+        lastMillis = currentMillis;
+        interval = 3000;      // Yellow light duration
+      }
+      break;
 
-      case yellow:
-        digitalWrite(12, HIGH);  // Keep green on
-        digitalWrite(4, HIGH);   // Turn on yellow warning light
-        if (currentMillis - lastMillis >= yellowInterval) {
-          lastMillis = currentMillis;  // Store the time of the event
-          digitalWrite(4, LOW);    // Turn off yellow warning light
-          if (set == 1) {
-            status = red;
-          } 
-          if (set == 0) {
-            status = green;
-          }
-        }
-        break;
+    case yellow:
+      if (digitalRead(4) == LOW) {
+        digitalWrite(4, HIGH); // Turn on traffic yellow light
+        lastMillis = currentMillis;
+      }
+      if (currentMillis - lastMillis >= interval) {
+        digitalWrite(4, LOW); // Turn off traffic yellow light
+        status = red;         // Transition to red state
+        lastMillis = currentMillis;
+        interval = 10000;     // Red light duration
+      }
+      break;
 
-      case red:
-        digitalWrite(12, HIGH);  // Keep green on
-        digitalWrite(6, HIGH);   // Turn on red light
-        if (currentMillis - lastMillis >= redInterval) {
-          lastMillis = currentMillis;  // Store the time of the event
-          digitalWrite(6, LOW);    // Turn off red light
-          status = yellow;
-          set = 0;  // Reset flag for next cycle
-        }
-        break;
-    }
+    case red:
+      if (digitalRead(6) == LOW) {
+        digitalWrite(6, HIGH); // Turn on traffic red light
+        lastMillis = currentMillis;
+      }
+      if (currentMillis - lastMillis >= interval) {
+        digitalWrite(6, LOW); // Turn off traffic red light
+        status = green;       // Return to green state
+        lastMillis = currentMillis;
+        interval = 10000;     // Green light duration
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+void handlePedestrianLights(unsigned long currentMillis) {
+  switch (status) {
+    case pgreen:
+      if (currentMillis - lastMillis >= interval) {
+        digitalWrite(6, HIGH);  // Turn on traffic red light
+        digitalWrite(2, LOW);   // Turn off traffic green light
+        digitalWrite(4, LOW);   // Turn off traffic yellow light
+        digitalWrite(12, LOW); // Turn off pedestrian red light
+        digitalWrite(11, HIGH); // Turn on pedestrian green light
+        lastMillis = currentMillis;
+        interval = 10000;       // Pedestrian green light duration
+        status = pred;          // Transition to pedestrian red
+      }
+      break;
+
+    case pred:
+      if (currentMillis - lastMillis >= interval) {
+        digitalWrite(11, LOW);  // Turn off pedestrian green light
+        digitalWrite(12, HIGH); // Turn on pedestrian red light
+        pedestrianRequest = false; // Reset pedestrian request
+        status = green;         // Return to normal traffic light control
+        lastMillis = currentMillis;
+        interval = 10000;       // Reset interval for green
+      }
+      break;
+
+    default:
+      break;
   }
 }
